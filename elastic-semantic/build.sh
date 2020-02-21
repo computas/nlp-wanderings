7b3f3cef4f53f6ac1f42baf1a9b83503da2079df
Marco Bertani-Økland <mbertani@gmail.com>
2020-02-21 13:16:33 +0100

    fix elastic version

--- elastic-semantic/build.sh
+++ elastic-semantic/build.sh
@@ -1,4 +1,4 @@
-export ELASTIC_VERSION=7.6
+export ELASTIC_VERSION=7.6.0
 docker build -t "custom/elasticsearch:latest" --build-arg ELASTIC_VERSION=$ELASTIC_VERSION -f elasticsearch/Dockerfile .
 docker build -t "custom/kibana:latest" --build-arg ELASTIC_VERSION=$ELASTIC_VERSION -f kibana/Dockerfile .
 
--- elastic-semantic/stop.sh
+++ elastic-semantic/stop.sh
@@ -1,3 +1,3 @@
-export ELASTIC_VERSION=7.6
+export ELASTIC_VERSION=7.6.0
 
 docker-compose down
